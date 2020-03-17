8f4fa291760f98343eb11240f0e56afafad5ffdd
Bertrand Bichat <bertrand.bichat@gmail.com>
2020-03-17 13:58:13 +0100

    pages dashboard & uikit enable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home,:uikit, :dashboard]
 
   def home
   end
@@ -16,10 +16,10 @@ class PagesController < ApplicationController
   end
 end
 
-rails g model Boardgame name category players_min:integer players_max:integer age description:text user:references
+# rails g model Boardgame name category players_min:integer players_max:integer age description:text user:references
 
-rails g model Location name address longitude:integer latitude:integer user:references # integer pour lon & lat ???
+# rails g model Location name address longitude:integer latitude:integer user:references # integer pour lon & lat ???
 
-rails g model Gamenight date:date start_time:time end_time:time description:text location:references boardgame:references
+# rails g model Gamenight date:date start_time:time end_time:time description:text location:references boardgame:references
 
-rails g model Participation rating:integer description:text user:references gamenight:references
+# rails g model Participation rating:integer description:text user:references gamenight:references
