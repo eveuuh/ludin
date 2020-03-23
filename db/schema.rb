44268aecd2ca06ca9719f9e1233ae6eb8be9c999
eve jourdan <eve.jourdan@gmail.com>
2020-03-23 14:51:54 +0100

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_122628) do
+ActiveRecord::Schema.define(version: 2020_03_18_115323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,6 @@ ActiveRecord::Schema.define(version: 2020_03_23_122628) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "status", default: false, null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["boardgame_id"], name: "index_gamenights_on_boardgame_id"
     t.index ["location_id"], name: "index_gamenights_on_location_id"
   end
@@ -77,8 +75,6 @@ ActiveRecord::Schema.define(version: 2020_03_23_122628) do
     t.string "username"
     t.text "presentation"
     t.float "global_rating"
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
