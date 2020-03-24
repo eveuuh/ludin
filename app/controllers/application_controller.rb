3f8f5963ef3c12d2db870599cb4b6b036c1af559
kevin <kraynaud@gmail.com>
2020-03-24 15:15:12 +0100

    almost done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:username, :email, :password, :password_confirmation, :current_password, :avatar)
+      user_params.permit(:email, :password, :password_confirmation, :current_password)
     end
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit(:username, :email, :password, :password_confirmation, :current_password, :avatar)
+      user_params.permit(:username, :email, :password, :password_confirmation, :current_password, :photo)
     end
   end
 end
