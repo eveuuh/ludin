b0a74dc5ba9ec9504dc4c6447e38a4743ea3f683
kevin <kraynaud@gmail.com>
2020-04-29 16:47:45 +0200

    mailer finished

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'contact@ludin.fun'
+  default from: 'ludin.fun@gmail.com'
   layout 'mailer'
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
 
   def general_message(contact)
     @contact = contact
-    mail to: "ludin@gmail.com", subject: "You have a new message from LUD-IN website contact page"
+    mail to: "ludin.fun@gmail.com", subject: "You have a new message from LUD-IN website contact page"
   end
 end
